e71477da1b5341bfc2d741a6193a0773c4c3761c
Francisco Italo <italo.silvan87@gmail.com>
2022-06-18 14:43:41 -0300

    Adicionar Formik

--- pages/RegisterAttraction.tsx
+++ pages/RegisterAttraction.tsx
@@ -1,24 +1,52 @@
 import React, { useState } from "react";
-import { View, TextInput, Switch, StyleSheet } from "react-native";
+import { View, TextInput, Switch, StyleSheet,Text, Button, } from "react-native";
+import {Formik} from 'formik';
 
 export default function RegisterAttraction() {
   const [isEnabled, setIsEnabled] = useState(false);
   const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
 
   return (
-    <View style={style.container}>
-      <TextInput placeholder="Nome da atração" />
-      <Switch
-        trackColor={{ false: "red", true: "green" }}
-        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-        accessibilityLabel="Status"
-      />
-      <TextInput placeholder="descrição" />
-      <TextInput placeholder="duração" />
-    </View>
+
+    <Formik
+      initialValues={{name:'',status:false,description:'',duration:''}}
+      onSubmit={e =>console.log(e)}
+    >
+      {({ handleChange, handleBlur, handleSubmit, values })=>(
+        <View style={style.container}>
+        <View style={style.containerNameStatus}>
+        <TextInput placeholder="Nome da atração" onChangeText={handleChange('name')} onBlur={handleBlur('name')} value={values.name} />
+        <View style={style.status}>
+        <Text>atração {`${isEnabled?"Ativa":"Inativa"}`}</Text>
+        <Switch
+          trackColor={{ false: "red", true: "green" }}
+          thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={values.status}
+          accessibilityLabel="Status"
+          onChange={handleChange('status')}
+          
+        />
+        </View>
+        
+        </View>
+          <View style={style.infoSecundary}>
+          <TextInput placeholder="descrição" multiline={true} onChangeText={handleChange('description')} onBlur={handleBlur('description')} value={values.description}/>
+          <TextInput placeholder="duração" onChangeText={handleChange('duration')} onBlur={handleBlur('duration')} value={values.duration} />
+      
+          </View>
+  
+  
+          <Button
+           title="Cadastrar"
+           onPress={handleSubmit}
+          />
+        
+        </View> 
+
+      )}
+     </Formik>
   );
 }
 
@@ -27,5 +55,23 @@ const style = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    border:'1px solid black',
   },
+
+  containerNameStatus:{
+    flexDirection:"row",
+    justifyContent:'space-between',
+    alignItems:'center',
+  },
+  status:{
+    alignItems:'center',
+    //flexDirection:"row"
+
+  },
+  infoSecundary:{
+
+    flexDirection:"row",
+    justifyContent:"space-between",
+    
+  }
 });
